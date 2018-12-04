6960cfbcf7e76e7f167331501782370c8d31fb73
sisar4 <alessiocecchi2000@yahoo.it>
2018-12-04 10:01:39 +0100

    Commit a caso sul master

--- Stampa1-10/Origine.cpp
+++ Stampa1-10/Origine.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
 	cout << "Buon giorno!" << endl;
+	//il conflitto dovrebbe essere da queste parti
 	for (int iNum=1; iNum <= 10; iNum++)
 	{
 		cout << iNum << endl;
