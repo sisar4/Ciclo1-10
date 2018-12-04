f13a36b086f5e609640e5aab26f314af0fd73b5e
sisar4 <alessiocecchi2000@yahoo.it>
2018-12-04 09:53:51 +0100

    fix

--- Stampa1-10/Origine.cpp
+++ Stampa1-10/Origine.cpp
@@ -4,8 +4,11 @@ using namespace std;
 int main()
 {
 	cout << "Buon giorno!" << endl;
-	for (int iNum=0; iNum < 10; iNum++)
+	int iNum;
+	for (iNum = 0; iNum <= 10; iNum++)
 	{
 		cout << iNum << endl;
 	}
+	cout << "Arrivederci" << endl;
+	system("pause");
 }
